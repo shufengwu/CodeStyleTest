2f8deea545dd0e2f36c4cb259180a1be057858da
Shufeng.Wu <Shufeng.Wu@deltaww.com>
2017-07-27 17:46:26 +0800

    Initial commit

---
+++ src/com/delta.java
@@ -0,0 +1,17 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class delta {
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<>();
+        a.add("2");
+        a.add("1");
+        for (String temp : a) {
+            if ("1".equals(temp)) {
+                a.remove(temp);
+            }
+        }
+    }
+}
