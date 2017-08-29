0fc54079f9a83cedcdb8fe08550250af1e12e1a0
Shufeng.Wu <Shufeng.Wu@deltaww.com>
2017-08-29 18:04:32 +0800

    test

---
+++ src/com/delta/lineSeparator/LineSeparatorTest.java
@@ -0,0 +1,63 @@
+package com.delta.lineSeparator;
+
+public class LineSeparatorTest {
+    public static void main(String[] args) {
+
+
+        /*File file = new File("javaIoTestFile.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //写入数据
+        try {
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw=new BufferedWriter(fw);
+            bw.newLine();
+            bw.flush();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //读取数据
+        if (file.exists()) {
+            FileReader fileReader = null;
+            try {
+                fileReader = new FileReader(file);
+                int fileLen = (int) file.length();
+                char[] chars = new char[fileLen];
+                try {
+                    fileReader.read(chars);
+                    String txt = String.valueOf(chars);
+                    System.out.println("\r\n".equals(txt));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Done");
+        System.out.println();
+
+        String lineSeparator = System.getProperty("line.separator");
+        System.out.println(lineSeparator.length());
+        System.out.println("hehehehe" + lineSeparator + "testtest");
+        System.out.println("hehe\r");
+        System.out.println(lineSeparator.equals("\r\n"));
+        System.out.println();
+
+        String lineSeparator2 = System.lineSeparator();
+        System.out.println(lineSeparator2.length());
+        System.out.println("hehehehe2" + lineSeparator2 + "testtest2");
+        System.out.println(lineSeparator2.equals("\r\n"));
+        System.out.println();*/
+
+
+    }
+}
